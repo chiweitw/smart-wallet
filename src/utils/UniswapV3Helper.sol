cd152f71aeb03bbb33aaf95c49c0ddd7ddfadb7d
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2024-01-05 19:08:07 +0800

    UniswapV3Helper and test testSingleHop

---
+++ src/utils/UniswapV3Helper.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/console.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+contract UniswapV3Helper {
+    ISwapRouter public immutable uniswap;
+
+    constructor(ISwapRouter _uniswap) {
+        uniswap = _uniswap;
+    }
+
+    ISwapRouter constant router =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+    function swapExactInputSingleHop(
+        address tokenIn,
+        address tokenOut,
+        uint24 poolFee,
+        uint amountIn
+    ) external returns (uint amountOut) {
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(address(router), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: tokenIn,
+                tokenOut: tokenOut,
+                fee: poolFee,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = uniswap.exactInputSingle(params);
+    }
+
+    function swapExactInputMultiHop(
+        bytes calldata path,
+        address tokenIn,
+        uint amountIn
+    ) external returns (uint amountOut) {
+        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(address(uniswap), amountIn);
+
+        ISwapRouter.ExactInputParams memory params = ISwapRouter.ExactInputParams({
+            path: path,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: 0
+        });
+        amountOut = router.exactInput(params);
+    }
+}
