3a2f48385c0a599c54c65a14fa5eaa01f99571ce
chiwei.huang <chiwei.huang@chiweihuangdeMacBook-Air.local>
2024-01-07 17:21:04 +0800

    only admin can add or remove owner

--- test/OwnerManager.t.sol
+++ test/OwnerManager.t.sol
@@ -5,15 +5,22 @@ import { Test, console } from "forge-std/Test.sol";
 import { HelperTest } from "./Helper.t.sol";
 
 contract OwnerManagerTest is HelperTest {
+    // orginal owners = [alice, bob, carol], by default alice is the admin;
+    // original confirmationNum = 2;
     function testAddOwnerAndConfirmationNumber() public {
-        // orginal owners = [alice, bob, carol], by default alice is the admin;
-        // original confirmationNum = 2;
         vm.startPrank(alice);
         wallet.addOwnerAndConfirmationNumber(someone, 3);
         vm.stopPrank();
 
         assertEq(wallet.isOwner(someone), true);
         assertEq(wallet.confirmationNum(), 3);
+    }
+
+    function testOnlyAdminCanAddOwner() public {
+        vm.startPrank(bob);
+        vm.expectRevert("Only Admin");
+        wallet.addOwnerAndConfirmationNumber(someone, 3);
+        vm.stopPrank();
     }
 
     function testConfirmationNumberCannotMoreThanOwnerCountWhenAddOwner() public {
@@ -30,6 +37,13 @@ contract OwnerManagerTest is HelperTest {
 
         assertEq(wallet.isOwner(carol), false);
         assertEq(wallet.confirmationNum(), 1);
+    }
+
+    function testOnlyAdminCanRemoveOwner() public {
+        vm.startPrank(bob);
+        vm.expectRevert("Only Admin");
+        wallet.addOwnerAndConfirmationNumber(carol, 1);
+        vm.stopPrank();
     }
 
     function testConfirmationNumberCannotMoreThanOwnerCountWhenRemoveOwner() public {
